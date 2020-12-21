6090ac3ef41eae2d5d6441782cfabfbf0488de4b
yuan <gn2481@gmail.com>
2020-12-21 16:02:44 +0800

    can_change_quantity_in_cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,8 +1,25 @@
 class CartItemsController < ApplicationController
+  
+  def update
+    @item = CartItem.find_by(product_id: params[:id])
+      if @item.update(item_params)
+        redirect_back fallback_location: root_path, notice: '成功更改數量'
+      else
+        redirect_back fallback_location: root_path, alert: '請再試一次'
+      end
+  end
+  
+  
+  
   def destroy
     @item = CartItem.find(params[:id])
     @item.destroy
     redirect_back fallback_location: root_path, notice: '成功刪除商品'
+  end
+  
+  private
+  def item_params
+    params.require(:cart_item).permit(:quantity)
   end
 
 end
