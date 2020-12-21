9165bab1d7e45c34ce5c6861c95c89722d7f42d3
yuan <gn2481@gmail.com>
2020-12-21 15:41:00 +0800

    cannot_add_product_again

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :find_product,only:[:show, :add_to_cart]
   def index
     @products = Product.all
   end
@@ -8,8 +9,16 @@ class ProductsController < ApplicationController
   end
   
   def add_to_cart 
-    product = Product.find(params[:id])
-    current_cart.add_product_to_cart(product)
-    redirect_back fallback_location: root_path, notice: '成功加入購物車'
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      redirect_back fallback_location: root_path, notice: '成功加入購物車'
+    else
+      redirect_back fallback_location: root_path, alert: '你已擁有此商品，若要更改數量，請至購物車頁面。'
+    end
+  end
+
+  private
+  def find_product
+    @product = Product.find(params[:id])
   end
 end
